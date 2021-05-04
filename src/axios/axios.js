9e925fa6e01616b904bb9fb9ea7e143ddb44338c
unknown <sr.ska89@gmail.com>
2021-05-04 11:49:44 -0300

    modificando axios

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://laravel-api-back.herokuapp.com/api/",
-});
+  baseURL: "https://laravel-api-back.herokuapp.com/api/"
+})
 
-const sendEmail = async (data) => {
+const sendEmail = async (dataConsole) => {
   const response = await instance({
     method: "post",
     url: "contact/store",
-    data: data,
+    data: dataConsole,
   });
 
   return response;
