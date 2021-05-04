549f93d01e61450289b2d851b4344472402ab727
unknown <sr.ska89@gmail.com>
2021-05-04 11:56:52 -0300

    cambio en contact.js

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,117 +1,89 @@
-import React, { useState } from "react";
-import ContactImage from "../../assets/img/contact-image.png";
-import { Form, Button, Container, Row, Col, Alert } from "react-bootstrap";
-import { sendEmail } from "../../axios/axios";
+import {React, useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ContactImage from '../../img/contact-image.png'
+import { SendEmail } from "../../axios/index";
 
 
 
 const Contact = () => {
-  const [validated, setValidated] = useState(false);
-  const [formData, setFormData] = useState({});
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log("Se envio");
-    const form = e.currentTarget;
-    console.log(form.checkValidity());
+    const [validated, setValidate] = useState(false)
+    const [DataConsole, setDataConsole] = useState({})
 
-    if (form.checkValidity()) {
-      console.log(formData);
-      console.log("El form fue validado");
-      let responseData = await sendEmail(formData);
-      console.log(responseData.data);
-      form.reset();
-    } else {
-      console.log("Te olvidaste algo");
+
+    const functionOnChange = (event) => {
+            console.log('FUNCIONA')
+            console.log(event.target.name)
+            let datos = {}  //crea un objeto vacío 
+            datos[event.target.name] = event.target.value      //se le da el valor del event al objeto datos.name
+            setDataConsole( (prevState) =>{                    //crea una funcion para retornar lo de abajo. funcion para guardar datos en DataConsole
+            return {
+                ...prevState,  //hace una copia del estado anterior...
+                [event.target.name] : event.target.value  //crea un objeto para guardar el event en DataConsole y mostrarlo por consola 
+            }
+        } )
     }
-  };
 
-  const handleOnChange = (e) => {
-    console.log(e.target.name);
-    console.log(e.target.value);
-    // Vamos a crear nuestro objeto formData
-    // {
-    //   name: 'eze'
-    //   email: "sr.ska89@gmail.com"
-    //   phone: 123456789
-    //   message: 'sadas222dasadasdasd asd asd '
-    // }
-    setFormData((prevState) => {
-      return {
-        ...prevState,
-        [e.target.name]: e.target.value,
-      };
-    });
-  };
-  console.log(formData);
+        console.log(DataConsole)
 
-  return (
-    <section className="contact">
-      <Container>
-        <Row>
-          <Col md={12} lg={5}>
-            <h3>
-              Get in touch<br></br>
-              <span>We are hiring!</span>
-            </h3>
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
-              <Form.Group>
-                <Form.Control
-                  onChange={handleOnChange}
-                  required
-                  type="text"
-                  placeholder="Name"
-                  name="name"
-                />
-              </Form.Group>
+    const functionOnSubmit = async (event) => {
+        event.preventDefault();  
+        const form = event.currentTarget; //currentTarget sirve para poder usar la validación del form checkValidity()
+        console.log(form.checkValidity())  //checkValidity hace la validación, devuelve true o false dependiendo si se validó o no 
 
-              <Form.Group>
-                <Form.Control
-                  onChange={handleOnChange}
-                  required
-                  name="email"
-                  type="email"
-                  placeholder="Email"
-                />
-              </Form.Group>
 
-              <Form.Group>
-                <Form.Control
-                  onChange={handleOnChange}
-                  required
-                  name="phone"
-                  type="tel"
-                  placeholder="Phone"
-                />
-              </Form.Group>
+        if(form.checkValidity()) {  //si la validación fue correcta =
+            console.log(DataConsole)
 
-              <Form.Group controlId="formBasicTextArea">
-                <Form.Control
-                  onChange={handleOnChange}
-                  required
-                  name="message"
-                  as="textarea"
-                  rows={3}
-                  placeholder="Message"
-                />
-                <Form.Control.Feedback type="invalid">
-                  Por favor ingrese un mensaje
-                </Form.Control.Feedback>
-              </Form.Group>
+            let responseData = await SendEmail(DataConsole) //se crea una variable para enviar los datos de la api, SendEmail esta declarada en 'axios/index.js'
 
-              <Button variant="primary" type="submit">
-                Send
-              </Button>
-            </Form>
-            <br />
-          </Col>
-          <Col md={12} lg={7}>
-            <img src={ContactImage} alt="Contact" className="d-block w-100" />
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  );
-};
+            console.log(responseData)
+
+            form.reset() //limpia los input al enviar los datos 
+        }
+    }
+
+return (
+    <section className="Contact" id="Contact-button">
+        <Container>
+            <Row>
+                <Col md={12} lg={5}>
+                    <h3 className="TitleContact">
+                       Get in touch <br/>
+                       <span className="TitleContact-span"> We are hiring! </span> 
+                    </h3>
+                </Col>
+                <Form className="ContactForm" noValidate validated={validated} onSubmit={functionOnSubmit}>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Control type="text" placeholder="Name" name="name" onChange={functionOnChange}/>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Control type="email" placeholder="Email" name="email" onChange={functionOnChange}/>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Control type="phone" placeholder="Phone" name="phone" onChange={functionOnChange} />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Control type="text" placeholder="Message" name="message" className="textarea" onChange={functionOnChange}/>
+                    </Form.Group>
+                
+                    <Button variant="primary" type="submit" className="ButtonSend" >
+                        Send
+                    </Button>
+                </Form>
+                <Col md={12} lg={5}>
+                <img src={ContactImage} alt="Pic of a ballon" className="d-block w-100 imagen " />
+                </Col>
+            </Row>
+        </Container>
+    </section>    
+)
+}
 
 export default Contact;
